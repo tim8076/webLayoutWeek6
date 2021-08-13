218ca2ffcc073865351f288325b08955d6ce8cdb
shosetim <shosetim32@gmail.com>
2021-08-13 23:26:25 +0800

    完成所有頁面切版

--- app/assets/js/all.js
+++ app/assets/js/all.js
@@ -1,3 +1,18 @@
-$(function() {
-  console.log('Hello Bootstrap5');
+
+const swiper = new Swiper(".mySwiper", {
+  slidesPerView: 1,
+  spaceBetween: 30,
+  loop: true,
+  autoplay: {
+    delay: 2500,
+    disableOnInteraction: false,
+  },
+  breakpoints: {
+    576: {
+      slidesPerView: 2,
+    },
+    769: {
+      slidesPerView: 3,
+    },
+  }
 });
