72a81bc7abd1f42a5aa5c6ca98e9e7a16b60f1c2
shosetim <shosetim32@gmail.com>
2021-08-14 20:54:20 +0800

    Update 2021-08-14T12:54:11.250Z

--- assets/js/all.js
+++ assets/js/all.js
@@ -1,24 +1,25 @@
+"use strict";
 
-const swiper = new Swiper(".mySwiper", {
+var swiper = new Swiper(".mySwiper", {
   slidesPerView: 1,
   spaceBetween: 30,
   loop: true,
   autoplay: {
     delay: 2500,
-    disableOnInteraction: false,
+    disableOnInteraction: false
   },
   breakpoints: {
     576: {
-      slidesPerView: 2,
+      slidesPerView: 2
     },
     769: {
-      slidesPerView: 3,
-    },
+      slidesPerView: 3
+    }
   }
 });
-
 AOS.init({
   offset: 140,
   duration: 1500,
   once: true
-})
+});
+//# sourceMappingURL=all.js.map
